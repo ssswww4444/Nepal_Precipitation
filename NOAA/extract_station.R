d56f5680250b908456d7b45c70024dd087412b7a
Pei-Yun Sun <peiyuns@student.unimelb.edu.au>
2023-04-22 23:11:27 +1000

    Update extract_station.R

--- NOAA/extract_station.R
+++ NOAA/extract_station.R
@@ -14,5 +14,5 @@ for (i in 1:dim(stations)[1]) {
   url <- paste0("https://coastwatch.pfeg.noaa.gov/erddap/griddap/chirps20GlobalDailyP05.csv?precip%5B(",
                 t1, "T00:00:00Z):1:(", t2, "T00:00:00Z)%5D%5B(",
                 lat1, "):1:(", lat2, ")%5D%5B(", lon1, "):1:(", lon2, ")%5D")
-  download.file(url=url, destfile=sprintf("data/%s.csv", name))
+  download.file(url=url, destfile=sprintf("data_station/%s.csv", name))
 }
